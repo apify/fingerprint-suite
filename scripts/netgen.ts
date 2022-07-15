3d3cb8984c2fbcb37a6862591e8be1bee9281e5b
Jindřich Bär <jindrichbar@gmail.com>
2022-07-15 11:30:21 +0200

    feat: experimental preprocessing (screen size)

--- scripts/netgen.ts
+++ scripts/netgen.ts
@@ -1,8 +1,20 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import { GeneratorNetworksCreator } from 'generator-networks-creator';
+import { readFileSync, writeFileSync } from 'fs';
 
 (async () => {
     const generatorNetworksCreator = new GeneratorNetworksCreator();
+
+    const fingerprintDataset = JSON.parse(readFileSync(`${__dirname}/dataset.json`).toString());
+
+    writeFileSync(`${__dirname}/dataset.json`,
+        JSON.stringify(
+            fingerprintDataset.filter(
+                (f:any) => f.browserFingerprint.screen.width >= 1280
+                    || f.browserFingerprint.screen.width < f.browserFingerprint.screen.height),
+        ),
+    );
+
     await generatorNetworksCreator.prepareFingerprintGeneratorFiles(
         `${__dirname}/dataset.json`,
         `${__dirname}/../packages/fingerprint-generator/src/data_files`,
