46d683b17d615e2bb78d62672625034376d80ef7
Jindřich Bär <jindrichbar@gmail.com>
2023-02-23 16:45:37 +0100

    fix: improve monorepo versioning + simplify publish

---
+++ scripts/publish.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PACKAGES_TO_PUBLISH=( "fingerprint-injector" "fingerprint-generator" "header-generator" "generative-bayesian-network" )
+
+for FOLDER in $SCRIPT_DIR/../packages/*; do
+  if [[ -d $FOLDER ]]; then
+    PACKAGE_NAME=$(basename $FOLDER)
+    if [[ " ${PACKAGES_TO_PUBLISH[@]} " =~ " ${PACKAGE_NAME} " ]]; then
+      echo "Publishing $PACKAGE_NAME"
+      cd $FOLDER
+      npm publish
+      cd ../../
+    fi
+  fi
+done
