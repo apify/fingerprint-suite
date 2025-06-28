a4248465be2393d94df4b5c9f66203cd145934c5
Alex <alex@jaralex.ee>
2025-06-28 17:39:37 +0300

    chore: initital

---
+++ test/reference/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  root: './static',
+  server: {
+    port: 3000
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'static/index.html')
+      }
+    }
+  }
+});
